9744fe23515a875f8cc7798cc6b8d2c2e70d0f38
Mapachana <anabuenrua99@gmail.com>
2018-08-09 21:27:44 +0200

    trying to adjust column as instances, almost failed

--- mini_columnas/columnas.gd
+++ mini_columnas/columnas.gd
@@ -3,7 +3,11 @@ extends Node2D
 var vel = Vector2(0.0,0.0)
 var speed = 2.0
 
-var anim 
+var anim
+
+var tiempo = 0
+
+var col_class = preload("res://mini_columnas/colum.tscn")
 
 func moverse():
 	if (Input.is_action_pressed("ui_right")):
@@ -15,14 +19,32 @@ func moverse():
 	else :
 		vel.x = 0
 		$player/AnimationPlayer.stop(true)
+		
+func add_columna(tipo):
+	var columna = col_class.instance()
+	add_child(columna)
+	columna.get_node("Sprite").frame = tipo
+	#(randf() * (y-x)) + x
+	columna.position.x = (randf()*(200))
+	
 
 func _ready():
+	randomize()
+	#var columna = col_class.instance()
+	#add_child(columna)
+	
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	pass
 
 func _process(delta):
 	moverse()
+	tiempo += delta
+	if (tiempo > 2):
+		add_columna(2)
+		tiempo = 0
+		
+	
 	
 	#get_node("player").move_and_collide(vel)
 	$player.move_and_collide(vel)
