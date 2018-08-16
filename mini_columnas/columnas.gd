803be1caad67954d6378f48c8e388e49c2d5edc6
Mapachana <anabuenrua99@gmail.com>
2018-08-16 21:21:23 +0200

    Columns game almost finished, it is only missing the death animation and the background and decorations

--- mini_columnas/columnas.gd
+++ mini_columnas/columnas.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
 var vel = Vector2(0.0,0.0)
-var speed = 2.0
+var speed = 2.0 #velocidad a la que se mueve
 
-var anim
+var anim #variable para animacion
 
-var tiempo = 0
+var tiempo = 0 #contador de tiempo
 
+#instancia de la clase columna para añadirla a la escena actual
 var col_class = preload("res://mini_columnas/colum.tscn")
 
 func moverse():
@@ -22,30 +23,37 @@ func moverse():
 		
 func add_columna(tipo):
 	var columna = col_class.instance()
+	columna.position.x = (randf()*(320)-160)
+	columna.position.y = -100
 	add_child(columna)
 	columna.get_node("Sprite").frame = tipo
 	#(randf() * (y-x)) + x
-	columna.position.x = (randf()*(200))
+
+	
+
 	
 
 func _ready():
-	randomize()
+	randomize() #genera la semilla para las cosas aleatorias
 	#var columna = col_class.instance()
 	#add_child(columna)
 	
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
+	
+	#set_process(false)
+	#set_physics_process(true)
 	pass
 
 func _process(delta):
 	moverse()
 	tiempo += delta
-	if (tiempo > 2):
-		add_columna(2)
+	if (tiempo > 1):
+		if (randf()>= 0.6):
+			add_columna(2)
+		else:
+			add_columna(1)
 		tiempo = 0
-		
-	
-	
 	#get_node("player").move_and_collide(vel)
 	$player.move_and_collide(vel)
 #	# Called every frame. Delta is time since last frame.
