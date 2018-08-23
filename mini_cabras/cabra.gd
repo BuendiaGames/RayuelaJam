43717a5f2b93c24d4ebb03a2e80444cb2b16a221
Mapachana <anabuenrua99@gmail.com>
2018-08-23 21:14:55 +0200

    finished the best minigame with goats ever. LOOVE GOATS

---
+++ mini_cabras/cabra.gd
@@ -0,0 +1,80 @@
+extends KinematicBody2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+var speed = 1.5
+
+var velocidad = Vector2(0.0,0.0)
+var aceleracion = Vector2(0.0,0.0)
+
+var tiempotrayectoria = 0
+
+var anim
+
+var player
+
+var encerrada = false
+
+
+func change_anim(new_anim):
+	if (new_anim != anim):
+		anim = new_anim
+		$AnimationPlayer.play(new_anim)
+
+func eliminar_cabra (): #Sacrificio debe ser una cabra
+	if (position.x < -300 or position.x > 300 or position.y < -200 or position.y > 200):
+		queue_free()
+
+func set_player (p):
+	player = p
+
+func fuerza_repulsiva (delta):
+	var r2 = position.distance_squared_to(player.position)
+	var f = 1.0/(r2/25000)
+	var n = position - player.position
+	n = n.normalized()
+	aceleracion = f*n
+	velocidad += aceleracion*delta
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+func _process(delta):
+	move_and_collide(velocidad)
+	if (velocidad.x != 0 or velocidad.y != 0):
+		change_anim("walk")
+		if (velocidad.x > 0):
+			$Sprite.flip_h = false
+		else:
+			$Sprite.flip_h = true
+	else:
+		$AnimationPlayer.stop(true)
+		anim = ""
+		
+	tiempotrayectoria += delta
+	
+	if (tiempotrayectoria >= 1):
+		var dir = randf()
+		if (dir < 0.25):
+			velocidad.x = speed
+			velocidad.y = 0
+		elif (dir < 0.5):
+			velocidad.x = -speed
+			velocidad.y = 0
+		elif (dir < 0.75):
+			velocidad.x = 0
+			velocidad.y = -speed
+		else :
+			velocidad.x = 0
+			velocidad.y = speed
+		tiempotrayectoria = 0
+	
+	fuerza_repulsiva(delta)
+	eliminar_cabra()
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+	pass
---
+++ mini_cabras/mini_cabras.gd
@@ -0,0 +1,93 @@
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+var rebaniocabras = preload("res://mini_cabras/cabra.tscn")
+
+var speed = 3
+var vel = Vector2(0.0,0.0)
+
+var tiempo = 0
+var tiempocabra = 3
+
+var anim
+
+var cabrasencerradas = 0
+
+func change_anim(new_anim):
+	if (new_anim != anim):
+		anim = new_anim
+		$player/AnimationPlayer.play(new_anim)
+
+func moverse():
+	if (Input.is_action_pressed("ui_right")):
+		vel.x = speed
+		vel.y = 0
+		change_anim("walk")
+	elif (Input.is_action_pressed("ui_left")):
+		vel.x = -speed
+		vel.y = 0
+		change_anim("walk2")
+	elif (Input.is_action_pressed("ui_up")):
+		vel.x = 0
+		vel.y = -speed
+		change_anim("down")
+	elif (Input.is_action_pressed("ui_down")):
+		vel.x = 0
+		vel.y = speed
+		change_anim("up")
+	else :
+		anim = ""
+		vel.x = 0
+		vel.y = 0
+		$player/AnimationPlayer.stop(true)
+		
+func add_cabra () :
+	var micabra = rebaniocabras.instance()
+	micabra.set_player($player)
+	var lado = randf()
+	if (lado < 0.5):
+		micabra.velocidad.x = micabra.speed
+		micabra.position.x = -240
+	else:
+		micabra.velocidad.x = - micabra.speed
+		micabra.position.x = 240
+	micabra.position.y = randf()*240 - 120
+	add_child(micabra)
+	
+func eliminar_cabra (sacrificio): #Sacrificio debe ser una cabra
+	if (sacrificio.position.x < -300 or sacrificio.position.x > 300 or sacrificio.position.y < -200 or sacrificio.position.y > 200):
+		sacrificio.queue_free()
+	if (sacrificio.encerrada):
+		sacrificio.queue_free()
+		cabrasencerradas += 1
+
+func _ready():
+	randomize()
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+func _process(delta):
+	tiempo += delta
+	moverse()
+	$player.move_and_collide(vel)
+	
+	if (tiempo >= tiempocabra):
+		add_cabra()
+		tiempo = 0
+	
+	
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func _on_Area2D_body_entered(body):
+	if (body.name != "player"):
+		body.queue_free()
+		cabrasencerradas += 1
+		print(cabrasencerradas)
+	pass # replace with function body
